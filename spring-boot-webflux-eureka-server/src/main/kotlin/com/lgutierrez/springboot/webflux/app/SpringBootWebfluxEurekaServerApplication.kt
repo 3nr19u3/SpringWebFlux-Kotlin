f25b47da918ac9cd6cb275ca2d0d318fc62942ef
3nr19u3 <kike123kob@gmail.com>
2024-10-21 20:16:24 -0500

    fourth try with update yml file to action on github

--- spring-boot-webflux-eureka-server/src/main/kotlin/com/lgutierrez/springboot/webflux/app/SpringBootWebfluxEurekaServerApplication.kt
+++ spring-boot-webflux-eureka-server/src/main/kotlin/com/lgutierrez/springboot/webflux/app/SpringBootWebfluxEurekaServerApplication.kt
@@ -2,8 +2,10 @@ package com.lgutierrez.springboot.webflux.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SpringBootWebfluxEurekaServerApplication
 
 fun main(args: Array<String>) {
